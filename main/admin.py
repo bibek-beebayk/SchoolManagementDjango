4a44ef0bd26a28f0a9ea8f2060839bf4919fe3c6
bibek-beebayk <beebayk0001@gmail.com>
2022-03-26 18:32:47 +0545

    Models: 1

--- main/admin.py
+++ main/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from . import models
 # Register your models here.
 
+admin.site.site_header = "MySchool"
+
 @admin.register(models.Teacher)
 class TeacherAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'middle_name', 'last_name','gender', 'salary', 'specialization']
@@ -18,4 +20,4 @@ class SubjectAdmin(admin.ModelAdmin):
 
 @admin.register(models.Student)
 class StudentAdmin(admin.ModelAdmin):
-    list_display = []
+    list_display = ['first_name', 'middle_name', 'last_name', 'enrolled_class', 'phone', 'address']
--- main/models.py
+++ main/models.py
@@ -1,20 +1,24 @@
 
+from ast import Sub
 from django.db import models
 
 
-    
+GENDER_CHOICES = [('M','Male'), ('F', 'Female'), ('O', 'Other')]   
 # Create your models here.
 class Teacher(models.Model):
-    first_name = models.CharField(max_length=20)
-    middle_name = models.CharField(max_length=20, blank=True, null=True)
-    last_name = models.CharField(max_length=20)
+
+    
+
+    first_name = models.CharField(max_length=256)
+    middle_name = models.CharField(max_length=56, blank=True, null=True)
+    last_name = models.CharField(max_length=256)
     mobile_number = models.CharField(max_length=10)
-    address = models.CharField(max_length=100)
+    address = models.CharField(max_length=256)
     email = models.EmailField()
     joining_date = models.DateField()
     salary = models.IntegerField()
-    gender = models.CharField(max_length=1, choices= [('M','Male'), ('F', 'Female'), ('O', 'Other')])
-    specialization = models.CharField(max_length=50)
+    gender = models.CharField(max_length=1, choices= GENDER_CHOICES )
+    specialization = models.CharField(max_length=256)
     dob = models.DateField()
 
     def __str__(self) -> str:
@@ -26,7 +30,7 @@ class Teacher(models.Model):
 
 
 class Class(models.Model): 
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=256)
     room_no = models.CharField(max_length=10)
     student_count = models.IntegerField()
     class_teacher = models.OneToOneField(Teacher, on_delete=models.SET_NULL, null=True)
@@ -37,30 +41,30 @@ class Class(models.Model):
 
 
 class Subject(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=256)
     subject_teacher = models.ManyToManyField(Teacher)
-    text_book = models.CharField(max_length=100, null=True)
+    text_book = models.CharField(max_length=256, null=True)
 
     def __str__(self) -> str:
         return self.name
 
 
 class Student(models.Model):
-    first_name = models.CharField(max_length=20)
-    middle_name = models.CharField(max_length=20, null=True, blank=True)
-    last_name = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=256)
+    middle_name = models.CharField(max_length=256, null=True, blank=True)
+    last_name = models.CharField(max_length=256)
     enrolled_class = models.ForeignKey(Class, on_delete=models.SET_NULL, null=True)
-    roll_no = models.IntegerField()
+    roll_no = models.PositiveSmallIntegerField() 
     admission_date = models.DateField()
-    address = models.CharField(max_length=100)
+    address = models.CharField(max_length=256)
     phone = models.CharField(max_length=10)
     email = models.EmailField()
     dob = models.DateField()
-    gender = models.CharField(max_length=1, choices=[('M', 'Male'), ('F', 'Female'), ('O', 'Other')])
-    previous_school = models.CharField(max_length=100, null=True, blank=True)
-    guardian_name = models.CharField(max_length=50)
-    guardian_profession = models.CharField(max_length=50)
-    guardian_address = models.CharField(max_length=50)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    previous_school = models.CharField(max_length=256, null=True, blank=True)
+    guardian_name = models.CharField(max_length=256)
+    guardian_profession = models.CharField(max_length=256)
+    guardian_address = models.CharField(max_length=256)
     guardian_phone = models.CharField(max_length=10)
     
     def __str__(self) -> str:
@@ -68,3 +72,35 @@ class Student(models.Model):
             return self.first_name +' '+ self.middle_name + ' ' + self.last_name
         else:
             return self.first_name + ' ' + self.last_name
+
+
+class Department(models.Model):
+    name = models.CharField(max_length=256)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Staff(models.Model):
+    first_name = models.CharField(max_length=256)
+    middle_name = models.CharField(max_length=256, null=True, blank=True)
+    last_name = models.CharField(max_length=256)
+    address = models.CharField(max_length=256)
+    phone = models.CharField(max_length=10)
+    email = models.EmailField()
+    job_designation = models.CharField(max_length=256)
+    salary = models.IntegerField()
+    joining_date = models.DateField()
+    dept = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self) -> str:
+        if self.middle_name:
+            return self.first_name +' '+ self.middle_name + ' ' + self.last_name
+        else:
+            return self.first_name + ' ' + self.last_name
+
+
+class Assignment(models.Model):
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    description = models.TextField()
+    submission_date = models.DateField()
+    # attachments = 
