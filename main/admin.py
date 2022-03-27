44e17a313a6d217ca6cecaa00b95ad910aa29f1c
bibek-beebayk <beebayk0001@gmail.com>
2022-03-27 20:14:52 +0545

    Built the models

--- main/admin.py
+++ main/admin.py
@@ -8,8 +8,8 @@ admin.site.site_header = "MySchool"
 class TeacherAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'middle_name', 'last_name','gender', 'salary', 'specialization']
 
-@admin.register(models.Class)
-class ClassAdmin(admin.ModelAdmin):
+@admin.register(models.Grade)
+class GradeAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'room_no', 'student_count', 'class_teacher']
 
 
@@ -21,3 +21,43 @@ class SubjectAdmin(admin.ModelAdmin):
 @admin.register(models.Student)
 class StudentAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'middle_name', 'last_name', 'enrolled_class', 'phone', 'address']
+
+@admin.register(models.Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+@admin.register(models.Staff)
+class StaffAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'middle_name', 'last_name', 'job_designation', 'joining_date', 'salary']
+
+@admin.register(models.Assignment)
+class AssignmentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'subject', 'grade', 'short_title', 'submission_date']
+
+@admin.register(models.Exam)
+class ExamAdmin(admin.ModelAdmin):
+    list_display = ['id', 'grade', 'subject', 'exam_date', 'start_time', 'room_no']
+
+
+@admin.register(models.Fee)
+class FeeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'student', 'admission_fee', 'monthly_fee', 'transportation_fee', 'other_fee', 'discount']
+
+# @admin.register(models.Result)
+# class ResultAdmin(admin.ModelAdmin):
+#     list_display = ['id', 'student', 'grade', 'total_marks']
+
+# class MarksInline(admin.StackedInline):
+#     model = models.Marks
+#     extra = 0
+#     fields = ['student']
+
+# @admin.register(models.Result)
+# class ResultAdmin(admin.ModelAdmin):
+#     inlines = [MarksInline]
+#     list_display = ['id', 'student']
+
+
+@admin.register(models.Result)
+class ResultAdmin(admin.ModelAdmin):
+    list_display = ['student', 'full_marks', 'pass_marks', 'obtained_marks', 'percentage', 'result'] 
--- main/models.py
+++ main/models.py
@@ -1,25 +1,22 @@
-
-from ast import Sub
+from email.policy import default
 from django.db import models
 
 
 GENDER_CHOICES = [('M','Male'), ('F', 'Female'), ('O', 'Other')]   
 # Create your models here.
-class Teacher(models.Model):
-
+class Teacher(models.Model): 
     
-
-    first_name = models.CharField(max_length=256)
-    middle_name = models.CharField(max_length=56, blank=True, null=True)
-    last_name = models.CharField(max_length=256)
+    first_name = models.CharField(max_length=256,verbose_name='First Name')
+    middle_name = models.CharField(max_length=56, blank=True, null=True, verbose_name='Middle Name')
+    last_name = models.CharField(max_length=256, verbose_name='Last Name')
     mobile_number = models.CharField(max_length=10)
     address = models.CharField(max_length=256)
     email = models.EmailField()
-    joining_date = models.DateField()
+    joining_date = models.DateField(verbose_name='Joining Date')
     salary = models.IntegerField()
     gender = models.CharField(max_length=1, choices= GENDER_CHOICES )
     specialization = models.CharField(max_length=256)
-    dob = models.DateField()
+    dob = models.DateField(verbose_name='Date of Birth')
 
     def __str__(self) -> str:
         if self.middle_name:
@@ -27,9 +24,13 @@ class Teacher(models.Model):
         else:
             return self.first_name + ' ' + self.last_name
 
+    class Meta:
+        db_table = 'Teachers'
+        verbose_name_plural = 'Teachers'
 
 
-class Class(models.Model): 
+
+class Grade(models.Model): 
     name = models.CharField(max_length=256)
     room_no = models.CharField(max_length=10)
     student_count = models.IntegerField()
@@ -38,34 +39,42 @@ class Class(models.Model):
     def __str__(self) -> str:
         return self.name
 
+    class Meta:
+        db_table = 'Classes'
+        verbose_name_plural = 'Classes'
 
 
 class Subject(models.Model):
     name = models.CharField(max_length=256)
-    subject_teacher = models.ManyToManyField(Teacher)
-    text_book = models.CharField(max_length=256, null=True)
+    subject_teacher = models.ManyToManyField(Teacher, verbose_name='Subject Teacher')
+    text_book = models.CharField(max_length=256, null=True, verbose_name='Text Book')
 
     def __str__(self) -> str:
         return self.name
 
+    class Meta:
+        verbose_name_plural = 'Subjects'
+        db_table = 'Subjects'
+
 
 class Student(models.Model):
-    first_name = models.CharField(max_length=256)
-    middle_name = models.CharField(max_length=256, null=True, blank=True)
-    last_name = models.CharField(max_length=256)
-    enrolled_class = models.ForeignKey(Class, on_delete=models.SET_NULL, null=True)
-    roll_no = models.PositiveSmallIntegerField() 
-    admission_date = models.DateField()
+    first_name = models.CharField(max_length=256, verbose_name='First Name')
+    middle_name = models.CharField(max_length=256, null=True, blank=True, verbose_name='Middle Name')
+    last_name = models.CharField(max_length=256, verbose_name='Last Name')
+    enrolled_class = models.ForeignKey(Grade, on_delete=models.SET_NULL, null=True, verbose_name='Enrolled Class')
+    roll_no = models.PositiveSmallIntegerField(verbose_name='Roll Number') 
+    admission_date = models.DateField(verbose_name='Admission Date')
     address = models.CharField(max_length=256)
     phone = models.CharField(max_length=10)
     email = models.EmailField()
-    dob = models.DateField()
+    dob = models.DateField(verbose_name='Date of Birth')
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
-    previous_school = models.CharField(max_length=256, null=True, blank=True)
-    guardian_name = models.CharField(max_length=256)
-    guardian_profession = models.CharField(max_length=256)
-    guardian_address = models.CharField(max_length=256)
-    guardian_phone = models.CharField(max_length=10)
+    previous_school = models.CharField(max_length=256, null=True, blank=True, verbose_name='Previous School (if any)')
+    guardian_name = models.CharField(max_length=256, verbose_name="Guardian's Name")
+    guardian_profession = models.CharField(max_length=256, verbose_name="Guardian's Profession")
+    guardian_address = models.CharField(max_length=256, verbose_name="Guardian's Address")
+    guardian_phone = models.CharField(max_length=10, verbose_name="Guardian's Phone Number")
+    is_specially_abled = models.BooleanField(default=False, verbose_name="Is the student specially abled?", null=True)
     
     def __str__(self) -> str:
         if self.middle_name:
@@ -73,24 +82,32 @@ class Student(models.Model):
         else:
             return self.first_name + ' ' + self.last_name
 
+    class Meta:
+        db_table = 'Students'
+        verbose_name_plural = 'Students'
 
 class Department(models.Model):
-    name = models.CharField(max_length=256)
+    name = models.CharField(max_length=256, verbose_name="Department Name")
 
     def __str__(self) -> str:
         return self.name
 
+    class Meta:
+        db_table = 'Departments'
+        verbose_name_plural = 'Departments'
+
+
 class Staff(models.Model):
-    first_name = models.CharField(max_length=256)
-    middle_name = models.CharField(max_length=256, null=True, blank=True)
-    last_name = models.CharField(max_length=256)
+    first_name = models.CharField(max_length=256, verbose_name="First Name")
+    middle_name = models.CharField(max_length=256, null=True, blank=True, verbose_name='Middle Name')
+    last_name = models.CharField(max_length=256, verbose_name = "Last Namr")
     address = models.CharField(max_length=256)
-    phone = models.CharField(max_length=10)
+    phone = models.CharField(max_length=10, verbose_name="Phone Number")
     email = models.EmailField()
-    job_designation = models.CharField(max_length=256)
+    job_designation = models.CharField(max_length=256, verbose_name="Job Designation")
     salary = models.IntegerField()
-    joining_date = models.DateField()
-    dept = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True)
+    joining_date = models.DateField(verbose_name="Joining Date")
+    dept = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True, verbose_name="Department")
 
     def __str__(self) -> str:
         if self.middle_name:
@@ -98,9 +115,111 @@ class Staff(models.Model):
         else:
             return self.first_name + ' ' + self.last_name
 
+    class Meta:
+        verbose_name_plural = 'Staffs'
+        db_table = 'Staffs'
+
 
 class Assignment(models.Model):
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
-    description = models.TextField()
-    submission_date = models.DateField()
+    grade = models.ForeignKey(Grade, on_delete=models.CASCADE, default=None)
+    short_title = models.CharField(max_length=256, null=True, verbose_name="Short Title")
+    description = models.TextField(null=True, blank=True)
+    submission_date = models.DateField(verbose_name = "Submission Due Date")
+    is_active = models.BooleanField(default=True)
     # attachments = 
+
+    def __str__(self) -> str:
+        return self.grade + ' ' + self.subject
+
+    class Meta:
+        verbose_name_plural = 'Assignments'
+        db_table = 'Assignments'
+
+class Exam(models.Model):
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    grade = models.ForeignKey(Grade, on_delete=models.CASCADE, null=True)
+    exam_date = models.DateField(verbose_name="Date of Exam")
+    start_time = models.TimeField(verbose_name="Starting Time")
+    end_time = models.TimeField(verbose_name="Ending Time", null=True)
+    full_marks = models.PositiveSmallIntegerField(verbose_name="Full Marks")
+    pass_marks = models.PositiveSmallIntegerField(verbose_name="Pass Marks")
+    room_no = models.CharField(max_length=256, verbose_name="Room Number")
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.grade + ' ' + self.subject
+
+    class Meta:
+        verbose_name_plural = 'Exams'
+        db_table = 'Exams'
+
+class Fee(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.PROTECT, null=True)
+    admission_fee = models.IntegerField(null=True, blank=True)
+    monthly_fee = models.IntegerField(null=True)
+    transportation_fee = models.IntegerField(null=True)
+    other_fee = models.IntegerField(null=True)
+    discount = models.IntegerField(null=True)
+
+    class Meta:
+        db_table = 'Fees'
+        verbose_name_plural = 'Fees'
+
+    # def __str__(self):
+    #     return self.student
+
+class Result(models.Model):
+    RESULT_CHOICES = [('P', 'Pass'), ('F', 'Fail')]
+    student = models.ForeignKey(Student, on_delete=models.PROTECT)
+    full_marks = models.IntegerField()
+    pass_marks = models.IntegerField()
+    obtained_marks = models.IntegerField(null=True)
+    percentage = models.PositiveSmallIntegerField()
+    result = models.CharField(max_length=1, choices=RESULT_CHOICES)
+
+    class Meta:
+        db_table = 'Results'
+        verbose_name_plural = 'Results'
+
+#     subject1 = models.ForeignKey(Subject, von_delete=models.SET_NULL, null=True, blank=True, related_name='+', verbose_name='Subject')
+#     subject1_marks = models.PositiveSmallIntegerField(default=0, verbose_name='Marks')
+
+#     subject2 = models.ForeignKey(Subject, on_delete=models.SET_NULL, null=True, blank=True, related_name='+', verbose_name='Subject')
+#     subject2_marks = models.PositiveSmallIntegerField(default=0, verbose_name='Marks')
+
+#     subject3 = models.ForeignKey(Subject, on_delete=models.SET_NULL, null=True, blank=True, related_name='+', verbose_name='Subject')
+#     subject3_marks = models.PositiveSmallIntegerField(default=0, verbose_name='Marks')
+
+#     subject4 = models.ForeignKey(Subject, on_delete=models.SET_NULL, null=True, blank=True, related_name='+', verbose_name='Subject')
+#     subject4_marks = models.PositiveSmallIntegerField(default=0, verbose_name='Marks')
+
+#     subject5 = models.ForeignKey(Subject, on_delete=models.SET_NULL, null=True, blank=True, related_name='+', verbose_name='Subject')
+#     subject5_marks = models.PositiveSmallIntegerField(default=0, verbose_name='Marks')
+
+#     subject6 = models.ForeignKey(Subject, on_delete=models.SET_NULL, null=True, blank=True, related_name='+', verbose_name='Subject')
+#     subject6_marks = models.PositiveSmallIntegerField(default=0, verbose_name='Marks')
+
+#     subject7 = models.ForeignKey(Subject, on_delete=models.SET_NULL, null=True, blank=True, related_name='+', verbose_name='Subject')
+#     subject7_marks = models.PositiveSmallIntegerField(default=0, verbose_name='Marks')
+
+#     subject8 = models.ForeignKey(Subject, on_delete=models.SET_NULL, null=True, blank=True, related_name='+', verbose_name='Subject')
+#     subject8_marks = models.PositiveSmallIntegerField(default=0, verbose_name='Marks')
+
+#     def total_marks():
+#         total = 0
+
+    
+
+
+    
+
+# class Result(models.Model):
+#     student = models.ForeignKey(Student, on_delete=models.PROTECT)
+#     # obtained_marks = models.ForeignKey('Marks', on_delete=models.PROTECT, related_name ='+')
+
+# class Marks(models.Model):
+#     subject = models.ForeignKey(Subject, on_delete=models.PROTECT)
+#     marks = models.PositiveSmallIntegerField()
+#     result = models.ForeignKey(Result, on_delete=models.CASCADE, default=None)
+
